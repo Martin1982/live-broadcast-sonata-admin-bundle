79488d0ce96b498fe346fbe3376707e268414fb2
Martin de Keijzer <martin@spinninrecords.nl>
2020-08-04 11:13:22 +0200

    Move admin config to an extension

---
+++ DependencyInjection/LiveBroadcastSonataAdminExtension.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of martin1982/live-broadcast-sonata-admin-bundle which is released under MIT.
+ * See https://opensource.org/licenses/MIT for full license details.
+ */
+namespace Martin1982\LiveBroadcastSonataAdminBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+/**
+ * Class LiveBroadcastSonataAdminExtension
+ */
+class LiveBroadcastSonataAdminExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('admin.php');
+    }
+}
---
+++ Resources/config/admin.php
@@ -0,0 +1,99 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of martin1982/live-broadcast-sonata-admin-bundle which is released under MIT.
+ * See https://opensource.org/licenses/MIT for full license details.
+ */
+
+namespace Martin1982\LiveBroadcastSonataAdminBundle\DependencyInjection\Loader\Configurator;
+
+use Martin1982\LiveBroadcastBundle\Entity\Channel\AbstractChannel;
+use Martin1982\LiveBroadcastBundle\Entity\Channel\ChannelFacebook;
+use Martin1982\LiveBroadcastBundle\Entity\Channel\ChannelTwitch;
+use Martin1982\LiveBroadcastBundle\Entity\Channel\ChannelYouTube;
+use Martin1982\LiveBroadcastBundle\Entity\LiveBroadcast;
+use Martin1982\LiveBroadcastBundle\Entity\Media\AbstractMedia;
+use Martin1982\LiveBroadcastBundle\Entity\Media\MediaFile;
+use Martin1982\LiveBroadcastBundle\Entity\Media\MediaRtmp;
+use Martin1982\LiveBroadcastBundle\Entity\Media\MediaUrl;
+use Martin1982\LiveBroadcastSonataAdminBundle\Admin\Block\FacebookBlockService;
+use Martin1982\LiveBroadcastSonataAdminBundle\Admin\Block\YouTubeBlockService;
+use Martin1982\LiveBroadcastSonataAdminBundle\Admin\ChannelAdmin;
+use Martin1982\LiveBroadcastSonataAdminBundle\Admin\InputAdmin;
+use Martin1982\LiveBroadcastSonataAdminBundle\Admin\LiveBroadcastAdmin;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+/**
+ * Return configuration closure
+ *
+ * @param ContainerConfigurator $container
+ */
+return static function (ContainerConfigurator $container) {
+    $services = $container->services();
+    $services->set('sonata.admin.livebroadcast', LiveBroadcastAdmin::class)
+        ->args([
+            null,
+            LiveBroadcast::class,
+            null,
+        ])
+        ->tag('sonata.admin', [
+            'manager_type' => 'orm',
+            'group' => 'Live',
+            'label' => 'Live Broadcasts',
+        ])
+        ->call('setTranslationDomain', [LiveBroadcastAdmin::class])
+        ->call('setThumbnailPath', ['%livebroadcast.thumbnail.web_path%']);
+
+    $services->set('sonata.admin.channel', ChannelAdmin::class)
+        ->args([
+            null,
+            AbstractChannel::class,
+            'LiveBroadcastSonataAdminBundle:CRUD',
+        ])
+        ->tag('sonata.admin', [
+            'manager_type' => 'orm',
+            'group' => 'Live',
+            'label' => 'Channels',
+        ])
+        ->call('setSubclassConfigs', ['%livebroadcast.config%'])
+        ->call('setConfiguredSubclasses', [[
+            'Twitch' => ChannelTwitch::class,
+            'Facebook' => ChannelFacebook::class,
+            'YouTube' => ChannelYouTube::class,
+        ]]);
+
+    $services->set('sonata.admin.streaminput', InputAdmin::class)
+        ->args([
+            null,
+            AbstractMedia::class,
+            null,
+        ])
+        ->tag('sonata.admin', [
+            'manager_type' => 'orm',
+            'group' => 'Live',
+            'label' => 'Inputs',
+        ])
+        ->call('setSubClasses', [[
+            'File' => MediaFile::class,
+            'URL' => MediaUrl::class,
+            'RTMP' => MediaRtmp::class,
+        ]]);
+
+    $services->set('sonata.block.service.youtubeauth', YouTubeBlockService::class)
+        ->args([
+            'sonata.block.service.youtubeauth',
+            '@templating.engine.twig',
+            '@live.broadcast.channel_api.client.google',
+            '@request_stack',
+        ])
+        ->tag('sonata.block');
+
+    $services->set('sonata.block.service.facebookauth', FacebookBlockService::class)
+        ->args([
+            'sonata.block.service.facebookauth',
+            '@templating.engine.twig',
+            '@live.broadcast.facebook_api.service',
+            '@sonata.admin.channel',
+        ])
+        ->tag('sonata.block');
+};
